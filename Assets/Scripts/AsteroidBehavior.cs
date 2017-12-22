7e45db9cdc2ba9118605e32fe3e699de7bc04dc5
Patrick Brisbine <patrickbrisbine@gmail.com>
2017-12-22 14:11:10 -0800

    Updates to license

--- Assets/Scripts/AsteroidBehavior.cs
+++ Assets/Scripts/AsteroidBehavior.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿/* 
+ * Space Shooter Copyright (C) Patrick Brisbine - All Rights Reserved
+ *
+ * Space Shooter is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ *
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-nc-nd/3.0/>.
+ *  
+ * Written by Patrick Brisbine December 2017
+ */
+ 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
